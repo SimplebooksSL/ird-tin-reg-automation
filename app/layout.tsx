35f9fa0b19fc02f884a3188fa0c6ecca4140797e
yohan-simplebooks <yohan.simplebooks@gmail.com>
2025-07-03 17:02:57 +0530

    feat: add RootLayout component with metadata and global styles

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "IRD TIN Registration",
+  description: "Developped by Simplebooks Technologies",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-poppins`}>{children}</body>
+    </html>
+  );
+}
