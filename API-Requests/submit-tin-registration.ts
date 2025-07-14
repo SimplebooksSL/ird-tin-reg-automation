59415230c9b15afc92b20ece39f3eeec98c8b719
yohan-simplebooks <yohan.simplebooks@gmail.com>
2025-07-14 11:56:47 +0530

    refactor: remove commented-out code and clean up imports in submitTinRegistration

--- API-Requests/submit-tin-registration.ts
+++ API-Requests/submit-tin-registration.ts
@@ -1,41 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { IrdSubmissionData, SubmissionResult } from "@/types/ird-types";
-// import axios, { AxiosError } from "axios";
-
-// const API_BASE_URL = "http://localhost:5000/api/v1/tin-registration";
-
-// export async function submitTinRegistration(
-//   data: IrdSubmissionData
-// ): Promise<SubmissionResult> {
-//   interface ErrorResponseData {
-//     message?: string;
-//     [key: string]: unknown;
-//   }
-
-//   try {
-//     const response = await axios.post<{ result: SubmissionResult }>(
-//       `${API_BASE_URL}/submit`,
-//       data,
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     );
-
-//     return response.data.result;
-//   } catch (error) {
-//     if (axios.isAxiosError(error)) {
-//       const err = error as AxiosError<ErrorResponseData>;
-//       console.error("API Error:", err.message, err.response?.data);
-//       throw new Error(err.response?.data?.message || "TIN submission failed.");
-//     } else {
-//       console.error("Unexpected Error:", error);
-//       throw error;
-//     }
-//   }
-// }
-
 import axios, { AxiosError } from "axios";
 import { IrdSubmissionData, SubmissionResult } from "../types/ird-types";
 
